44355e5cce8652afbc83599afeac8a3e7e0f289b
Dom <dominic@dombox.xyz>
2020-08-23 22:13:58 +0100

    0.3.1

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 require('./src/apm')
 
 const { createServer, createApp, createRouter } = require('./src/app')
+const { logger, componentLogger } = require('./src/logger')
 
 const middleware = require('./src/middleware')
 const handlers = require('./src/handlers')
-
-const { logger, componentLogger } = require('logger')
 
 module.exports = {
   createServer,
   createApp,
   createRouter,
-  middleware,
-  handlers,
   logger,
-  componentLogger
+  componentLogger,
+  middleware,
+  handlers
 }
