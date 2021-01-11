af26cd3fabc11e0569a3085d403b4544f79e7b2d
Dom <dominic@dombox.xyz>
2021-01-11 22:39:34 +0000

    0.3.5

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,15 @@
-const { generateLogger, componentLogger } = require('logger')
+const { generateLogger, defaultTransport } = require('logger')
+
+const level = process.env.LOG_LEVEL || 'info'
+const defaultMeta = { component: 'server' }
+
+const componentLogger = (meta) => {
+
+  meta = Object.assign({}, defaultMeta, meta || {})
+
+  return generateLogger(defaultTransport(), { meta, level })
+
+}
 
 const logger = componentLogger()
 
