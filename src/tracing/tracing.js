13b150be594248673f3bcf642ca4219d4f24a3f8
Dom <dominic@dombox.xyz>
2021-07-05 01:00:05 +0100

    0.7.5

--- src/tracing/tracing.js
+++ src/tracing/tracing.js
@@ -3,6 +3,7 @@ const api = require('@opentelemetry/api')
 const { registerInstrumentations } = require('@opentelemetry/instrumentation')
 const { NodeTracerProvider } = require('@opentelemetry/node')
 const { SimpleSpanProcessor } = require('@opentelemetry/tracing')
+const { ResourceAttributes } = require('@opentelemetry/semantic-conventions')
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger')
 const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin')
 const { KoaInstrumentation } = require('@opentelemetry/instrumentation-koa')
@@ -13,19 +14,23 @@ const tracerProvider = new NodeTracerProvider()
 const tracingMiddleware = (opts) => {
 
   opts = Object.assign({
+    serviceName: process.env.APP_NAME,
+    serviceVersion: process.env.APP_VERSION,
     exporter: 'jaeger',
     instrumentations: [],
-    serviceName: process.env.APP_NAME,
-    serviceVersion: process.env.APP_VERSION
+    tags: {}
   }, opts || {})
 
-  const { serviceVersion, serviceName } = opts
+  const serviceName = opts.serviceName
 
-  const tags = { serviceName, serviceVersion }
+  const tags = Object.assign({
+    [ResourceAttributes.SERVICE_NAME]: opts.serviceName,
+    [ResourceAttributes.SERVICE_VERSION]: opts.serviceVersion
+  }, opts.tags)
 
   const exporter = opts.exporter === 'jaeger'
-    ? new JaegerExporter({ serviceName, tags })
-    : new ZipkinExporter({ serviceName, tags })
+    ? new JaegerExporter({ tags })
+    : new ZipkinExporter({ tags })
 
   const defaultProcessor = new SimpleSpanProcessor(exporter)
 
